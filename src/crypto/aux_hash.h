2755761605a2c53fb040620c03cb1c4abb3fb4b1
psychocrypt <psychocryptHPC@gmail.com>
2018-08-21 23:24:17 +0200

    docu and check length of input hashes in test cases
    - check that the length fits 200bytes for test hashes
    - doxygen documentation for `aux_hash.h`

--- src/crypto/aux_hash.h
+++ src/crypto/aux_hash.h
@@ -42,17 +42,23 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 
 #pragma once
-#include <inttypes.h>
-
-// !! NB
-// Hash functions in this file were optimised to handle only 200 bytes long input. As such they
-// are not useable outside of PoW calculation. 
+#include <inttypes.h> 
 
 extern "C" {
 
-void blake256_hash(const uint8_t* data,uint8_t* hashval);
-void skein_hash(const uint8_t* data,uint8_t *hashval);
-void groestl_hash(const uint8_t* data,uint8_t* hashval);
-void jh_hash(const uint8_t* data,uint8_t* hashval);
+/** auxiliary hashing functions
+ * 
+ * @warning Hash functions were optimized to handle only 200 bytes long input. As such they
+ * are not useable outside of PoW calculation.
+ *
+ * @param data 200byte input data
+ * @param hashval 32byte hashed data
+ * @{
+ */
+void blake256_hash(const uint8_t* data, uint8_t* hashval);
+void skein_hash(const uint8_t* data, uint8_t *hashval);
+void groestl_hash(const uint8_t* data, uint8_t* hashval);
+void jh_hash(const uint8_t* data, uint8_t* hashval);
 
+///@}
 }
