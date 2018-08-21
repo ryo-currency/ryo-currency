de3d0d424b215daf0e3ce1cf40efcc70e104d44d
fireice-uk <fireice-uk@users.noreply.github.com>
2018-08-21 19:09:44 +0100

    keccak unroll

--- src/crypto/cn_slow_hash_soft.cpp
+++ src/crypto/cn_slow_hash_soft.cpp
@@ -563,7 +563,7 @@ void cn_slow_hash<MEMORY, ITER, VERSION>::software_hash(const void *in, size_t l
 
 	implode_scratchpad_soft();
 
-	keccakf(spad.as_uqword(), 24);
+	keccakf(spad.as_uqword());
 
 	switch(spad.as_byte(0) & 3)
 	{
