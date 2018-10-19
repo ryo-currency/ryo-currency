31738f56c10fc5ca63a1a7dc67fe72953309ac5c
fireice-uk <fireice-uk@users.noreply.github.com>
2018-10-19 18:52:30 +0100

    wallet_rcp_server: add create from viewkey

--- src/wallet/wallet_rpc_server.h
+++ src/wallet/wallet_rpc_server.h
@@ -153,6 +153,7 @@ class wallet_rpc_server : public epee::http_server_impl_base<wallet_rpc_server>
 	MAP_JON_RPC_WE("get_languages", on_get_languages, wallet_rpc::COMMAND_RPC_GET_LANGUAGES)
 	MAP_JON_RPC_WE("create_wallet", on_create_wallet, wallet_rpc::COMMAND_RPC_CREATE_WALLET)
 	MAP_JON_RPC_WE("restore_wallet", on_restore_wallet, wallet_rpc::COMMAND_RPC_RESTORE_WALLET)
+	MAP_JON_RPC_WE("restore_view_wallet", on_restore_view_wallet, wallet_rpc::COMMAND_RPC_RESTORE_VIEW_WALLET)
 	MAP_JON_RPC_WE("open_wallet", on_open_wallet, wallet_rpc::COMMAND_RPC_OPEN_WALLET)
 	MAP_JON_RPC_WE("close_wallet", on_close_wallet, wallet_rpc::COMMAND_RPC_CLOSE_WALLET)
 	MAP_JON_RPC_WE("is_multisig", on_is_multisig, wallet_rpc::COMMAND_RPC_IS_MULTISIG)
@@ -222,6 +223,7 @@ class wallet_rpc_server : public epee::http_server_impl_base<wallet_rpc_server>
 	bool on_get_languages(const wallet_rpc::COMMAND_RPC_GET_LANGUAGES::request &req, wallet_rpc::COMMAND_RPC_GET_LANGUAGES::response &res, epee::json_rpc::error &er);
 	bool on_create_wallet(const wallet_rpc::COMMAND_RPC_CREATE_WALLET::request &req, wallet_rpc::COMMAND_RPC_CREATE_WALLET::response &res, epee::json_rpc::error &er);
 	bool on_restore_wallet(const wallet_rpc::COMMAND_RPC_RESTORE_WALLET::request &req, wallet_rpc::COMMAND_RPC_RESTORE_WALLET::response &res, epee::json_rpc::error &er);
+	bool on_restore_view_wallet(const wallet_rpc::COMMAND_RPC_RESTORE_VIEW_WALLET::request &req, wallet_rpc::COMMAND_RPC_RESTORE_VIEW_WALLET::response &res, epee::json_rpc::error &er);
 	bool on_open_wallet(const wallet_rpc::COMMAND_RPC_OPEN_WALLET::request &req, wallet_rpc::COMMAND_RPC_OPEN_WALLET::response &res, epee::json_rpc::error &er);
 	bool on_close_wallet(const wallet_rpc::COMMAND_RPC_CLOSE_WALLET::request &req, wallet_rpc::COMMAND_RPC_CLOSE_WALLET::response &res, epee::json_rpc::error &er);
 	bool on_is_multisig(const wallet_rpc::COMMAND_RPC_IS_MULTISIG::request &req, wallet_rpc::COMMAND_RPC_IS_MULTISIG::response &res, epee::json_rpc::error &er);
